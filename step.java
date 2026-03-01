54ce91fa8b3c4029c9133cc3c26da290cd437670
ad1tyapratap <pratapaditya581@gmail.com>
2026-03-01 21:48:07 +0530

    UC5

--- step.java
+++ step.java
@@ -1,47 +1,17 @@
-class step {
-    public static void main(String args[]) {
-        String[] banner = new String[7];
-        banner[0] = String.join(" ",
-                " *****  ",
-                " *****  ",
-                " ****** ",
-                " *****  ");
+public class step {
 
-        banner[1] = String.join(" ",
-                "*     * ",
-                "*     * ",
-                "*     * ",
-                "*     * ");
+    public static void main(String[] args) {
 
-        banner[2] = String.join(" ",
-                "*     * ",
-                "*     * ",
-                "*     * ",
-                "*       ");
+        String[] banner = {
+            String.join("", " *****   ", "******   ", "*******  ", " ****** "),
+            String.join("", "*     *  ", "*     *  ", "*     *  ", " *      "),
+            String.join("", "*     *  ", "*     *  ", "*     *  ", " *      "),
+            String.join("", "*     *  ", "******   ", "*******  ", " ****** "),
+            String.join("", "*     *  ", "*        ","*        ", "      *"),
+            String.join("", "*     *  ", "*        ","*        ", "      *"),
+            String.join("", " *****   ", "*        ","*         ","****** ")
+        };
 
-        banner[3] = String.join(" ",
-                "*     * ",
-                "*     * ",
-                " ****** ",
-                " *****  ");
-
-        banner[4] = String.join(" ",
-                "*     * ",
-                "*     * ",
-                "*       ",
-                "      * ");
-
-        banner[5] = String.join(" ",
-                "*     * ",
-                "*     * ",
-                "*       ",
-                "*     * ");
-
-        banner[6] = String.join(" ",
-                " *****  ",
-                " *****  ",
-                "*       ",
-                " *****  ");
         for (String line : banner) {
             System.out.println(line);
         }
